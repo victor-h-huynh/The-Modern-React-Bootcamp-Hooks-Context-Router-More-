01c6ba6984b7bb1ffd6aa4c995748f7f08c44097
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-02-08 15:31:28 -0500

    jsx-conditionals

---
+++ 03_JSX_Conditionals/index.js
@@ -0,0 +1,28 @@
+function getNum() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+class NumPicker extends React.Component {
+  render() {
+    const num = getNum();
+    let msg;
+    if (num === 7) {
+      msg = (
+        <div>
+          <h2>Congrats you Win!</h2>
+          <img src="https://media2.giphy.com/media/KztT2c4u8mYYUiMKdJ/giphy.gif" />
+        </div>
+      );
+    } else {
+      msg = <h2>Sorry, try again!</h2>;
+    }
+    return (
+      <div>
+        <h1>Your number is: {num}</h1>
+        {msg}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<NumPicker />, document.getElementById("root"));
