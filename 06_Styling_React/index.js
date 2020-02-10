82e3b8b7ed565847cb2eff399fa05ac6da623fa2
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-02-10 18:03:18 -0500

    styling-react

---
+++ 06_Styling_React/index.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Slot Machines!</h1>
+        <Machine s1="X" s2="X" s3="X" />
+        <Machine s1="X" s2="Y" s3="Z" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
