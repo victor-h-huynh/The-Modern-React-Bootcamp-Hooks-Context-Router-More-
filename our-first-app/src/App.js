1df2fa03f332ca2f9cee26d45732f7202676f7c6
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-02-12 01:17:47 -0500

    dog

---
+++ our-first-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Dog from "./Dog";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Dog />
+      </div>
+    );
+  }
+}
+
+export default App;
