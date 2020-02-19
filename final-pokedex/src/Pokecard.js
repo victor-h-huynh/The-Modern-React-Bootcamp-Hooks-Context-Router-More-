e3b54d036e4ae0b8e7a4183ca835b3464f577c36
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-02-19 14:49:55 -0500

    pokecard basic styling pass down props

--- final-pokedex/src/Pokecard.js
+++ final-pokedex/src/Pokecard.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import "./Pokecard.css";
+
+const Poke_API =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 class Pokecard extends Component {
   render() {
-    return <h1>Pokecard!</h1>;
+    let imgSrc = `${Poke_API}${this.props.id}.png`;
+    return (
+      <div className="Pokecard">
+        <h1>{this.props.name}</h1>
+        <img src={imgSrc} alt="pokemon" />
+        <div>Type: {this.props.type}</div>
+        <div>EXP: {this.props.exp}</div>
+      </div>
+    );
   }
 }
 
