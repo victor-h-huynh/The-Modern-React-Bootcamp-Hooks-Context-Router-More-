a530f30fec942215e37a8b7350def9bce0003869
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-02-20 16:21:57 -0500

    click events, state, this.bind

--- initialize-state/src/App.js
+++ initialize-state/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import State from "./State";
 import Demo from "./Demo";
 import Rando from "./Random";
+import Button from "./Button";
+import BrokenClick from "./BrokenClick";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Rando maxNum={7} />
+      <BrokenClick />
     </div>
   );
 }
