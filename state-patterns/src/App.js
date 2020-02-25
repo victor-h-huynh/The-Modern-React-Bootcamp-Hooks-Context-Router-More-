6f5e24be0bfcd1ba1ea29e5c6d793821094df803
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-02-25 16:12:22 -0500

    mutating state

--- state-patterns/src/App.js
+++ state-patterns/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import ScoreKeeper from "./ScoreKeeper";
+import IconList from "./IconList";
 
 function App() {
   return (
     <div className="App">
       <h1>State Patterns</h1>
       <ScoreKeeper />
+      <IconList />
     </div>
   );
 }
