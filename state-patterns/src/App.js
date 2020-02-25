327e8ce2d46c9f6ed11f43244771d3b79c9d6310
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-02-25 14:42:09 -0500

    state-patterns

---
+++ state-patterns/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>State Patterns</h1>
+    </div>
+  );
+}
+
+export default App;
